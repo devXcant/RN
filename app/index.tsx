17a21b4ab13dc15ccabe3094286daedb6ea5181c
devxcant <devxcant@gmail.com>
2025-02-16 22:17:24 +0100

    index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function Index() {
+  const [searchText, setSearchText] = useState()
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
+      <Searchbar placeholder='search here...' onChangeText={(e) => {
+        e.target.setSearchText
+        e.preventDefault()
+      }} value={searchText} />
       <Text>Start</Text>
-    </View>
+    </SafeAreaView>
   );
 }
