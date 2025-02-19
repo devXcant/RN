8628f2044ced2cc38e76bbf5207de3207de9081f
devXcant <devxcant@gmail.com>
2025-02-19 09:05:57 +0100

    cleanups

--- app/src/components/features/components/restaurant-info.tsx
+++ app/src/components/features/components/restaurant-info.tsx
@@ -1,15 +1,19 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { Card } from "react-native-paper";
-import styled from "styled-components";
+import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../../assets/svg/star.js";
 import open from "../../../../../assets/svg/open.js";
 import React from "react";
-import Spacer from "../../spacer.components.js";
 
 const Title = styled(Text)`
   font-family: ${(props: any) => props.theme.fonts.body};
-
   font-size: ${(props: any) => props.theme.fontSizes.body};
   font-weight: bold;
   color: ${(props: any) => props.theme.colors.ui.success};
@@ -33,36 +37,63 @@ const Rating = styled(View)`
 `;
 
 const Section = styled(View)`
- flex-direction: row;
- align-items: center;
-`
+  flex-direction: row;
+  align-items: center;
+`;
 
 const SectionEnd = styled(View)`
-flex: 1;
- flex-direction: row;
- justify-content: flex-end;
-`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
 
+interface Restaurant {
+  name: string;
+  icon: string;
+  photos: ImageSourcePropType[];
+  address: string;
+  isOpenNow: boolean;
+  rating: number;
+  isClosedTemporarily: boolean;
+}
 
+interface RestaurantInfoCardProps {
+  restaurant?: Restaurant;
+}
 
-const RestaurantInfoCard = ({ restaurant = {} }) => {
+const defaultRestaurant: Restaurant = {
+  name: "Some Restaurant",
+  icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+  photos: [
+    require("../../../../../assets/images/restaurant1.jpg"),
+    require("../../../../../assets/images/restaurant2.jpg"),
+    require("../../../../../assets/images/restaurant3.jpg"),
+  ],
+  address: "123 Random Street",
+  isOpenNow: true,
+  rating: 4,
+  isClosedTemporarily: true,
+};
+
+const RestaurantInfoCard: React.FC<RestaurantInfoCardProps> = ({
+  restaurant = defaultRestaurant
+}) => {
+  if (!restaurant) {
+    return null;
+  }
+
   const {
-    name = "Some Restaurant",
-    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
-    photos = [
-      require("../../../../../assets/images/restaurant1.jpg"),
-      require("../../../../../assets/images/restaurant2.jpg"),
-      require("../../../../../assets/images/restaurant3.jpg"),
-    ],
-    address = "123 Random Street",
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily = true,
+    name,
+    icon,
+    photos,
+    address,
+    isOpenNow,
+    rating,
+    isClosedTemporarily,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.ceil(rating)));
 
-  // Use the first photo if available; otherwise, fallback to a default image
   const photoSource =
     photos.length > 0 && photos[0]
       ? photos[0]
@@ -74,33 +105,24 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
       <Info>
         <Title>{name}</Title>
         <Section>
-
-        <Rating>
-          {ratingArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-
+          <Rating>
+            {ratingArray.map((_, index) => (
+              <SvgXml key={`star-${index}`} xml={star} width={20} height={20} />
+            ))}
           </Rating>
           <SectionEnd>
-            {isClosedTemporarily && (
-              <Text  style={{ color: "red" }}>
-                CLOSED TEMPORARILY
-              </Text>
-            )}
-            <Spacer variant="left.large" />
-
+            {isClosedTemporarily && <Text>CLOSED TEMPORARILY</Text>}
+            <View style={{ marginLeft: 16 }} />
             {isOpenNow && <SvgXml xml={open} width={16} height={16} />}
-            <Spacer variant="left.large" />
-            <Image style={{width: 15, height: 15}} source={{uri: icon}} />
+            <View style={{ marginLeft: 16 }} />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
-          </Section>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </Card>
   );
 };
-
-export default RestaurantInfoCard;
 
 const styles = StyleSheet.create({
   card: {
@@ -111,3 +133,5 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
 });
+
+export default RestaurantInfoCard;
