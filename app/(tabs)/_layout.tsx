c710ade4a329f749a53b134decfab08876816b84
devXcant <devxcant@gmail.com>
2025-02-21 07:34:45 +0100

    cleanups

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,58 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { useColorScheme } from "../hooks/useColorScheme.web";
+import { HapticTab } from "@/app-example/components/HapticTab";
+import BlurTabBarBackground from "@/app-example/components/ui/TabBarBackground.ios";
+import { Colors } from "@/app-example/constants/Colors";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: BlurTabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="maps"
+        options={{
+          title: "Maps",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Image, StyleSheet, Platform, SafeAreaView, Text } from "react-native";
+
+// import { HelloWave } from "@/components/HelloWave";
+// import ParallaxScrollView from "@/components/ParallaxScrollView";
+// import { ThemedText } from "@/components/ThemedText";
+// import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Main page xxx</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
